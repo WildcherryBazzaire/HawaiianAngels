bdd9cbb28a9330dc54228381b3b31767c042e614
Sum-Tim <timsumalinogiii@gmail.com>
2019-06-08 21:11:52 -1000

    working on the investor page add on

---
+++ components/investors/plan.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-row class="justify-content-center">
+    <b-col>
+      <h2>{{ content[0].planName }}</h2>
+      <img :src="content[0].image" />
+    </b-col>
+    <div class="divider"></div>
+    <b-col>
+      <h2>{{ content[1].planName }}</h2>
+      <img :src="content[1].image" />
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: [
+        {
+          planName: 'Reoccuring',
+          image: require('@/assets/investors/reoccuring.svg')
+        },
+        {
+          planName: 'One Time',
+          image: require('@/assets/investors/one_time.svg')
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.divider {
+  border-left: thick solid #ff0000;
+}
+</style>
