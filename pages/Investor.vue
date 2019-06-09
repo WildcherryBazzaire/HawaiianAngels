ac64deb8700104134a7f0d7b4c6b37aadd37d3f9
Alex <eieghtbitdolphin@gmail.com>
2019-06-08 17:59:38 -1000

    lol, small stuff

--- pages/Investor.vue
+++ pages/Investor.vue
@@ -15,7 +15,14 @@
         <span class="lines"></span>
       </b-col>
       <b-col sm="12" class="page-sub-headers text-sm-center">
+        <!--This the plan subheader -->
         <h2>1. Pick A Plan</h2>
+      </b-col>
+      <b-col sm="12">
+        <!--Plans will have their own interwoven row in this column -->
+        <b-row>
+          <b-col> </b-col>
+        </b-row>
       </b-col>
     </b-row>
   </b-container>
